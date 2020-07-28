5a43816dfec72819617f4670c3cc2999ec037112
shubham kumar verma <34179123+shubham-skv@users.noreply.github.com>
2020-07-29 01:54:43 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "cybercellshubham@gmail.com"
+receiver_email = "khushabusingh980@gmail.com"
+password ="K20NoWn43JOB"
+#input("Type your password and press enter:")
+message = """\
+Subject: Hi there
+I am shubham and this is a mail sent from my python mail client
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
